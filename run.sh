77639af31d0afb4b6537b5773cdac3eac3f3be66
Kartik Agarwala <agarwala.kartik@gmail.com>
2024-04-07 09:41:51 +0530

    ninja/meson are not installed, install them

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 # Update this file to run your own code
 
 # install dependencies
-meson setup build
+apt-get update && apt-get install meson ninja libsecp256k1-dev llvm-18 -y
+
 meson wrap install nlohmann_json
+meson setup build
 cd build
 
 # compile and execute script
