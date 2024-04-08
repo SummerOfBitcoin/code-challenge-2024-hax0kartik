6952f0d779c1711682194f083f5f041015c478ff
Kartik Agarwala <agarwala.kartik@gmail.com>
2024-04-08 09:51:22 +0530

    Try using gcc/g++ as the compiler in CI build

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # Update this file to run your own code
 
 # install dependencies
-wget https://apt.llvm.org/llvm.sh && chmod +x llvm.sh && sudo ./llvm.sh 18
-sudo apt-get update && sudo apt-get install meson ninja-build libsecp256k1-dev -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt-get install gcc-13 g++-13 -y
+sudo apt-get install meson ninja-build libsecp256k1-dev -y
 
 mkdir subprojects
 meson wrap install nlohmann_json && meson setup build && cd build && ninja SoB && ./SoB
