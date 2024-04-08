63a9aa79c2b5dc5ffdd1395c70f68ebe5b2f75b0
Kartik Agarwala <agarwala.kartik@gmail.com>
2024-04-08 14:12:06 +0530

    Try fixing CI

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 # Update this file to run your own code
 
 # install dependencies
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt-get install gcc-13 g++-13 -y
-sudo apt-get install meson ninja-build libsecp256k1-dev -y
-
+wget https://apt.llvm.org/llvm.sh && chmod +x llvm.sh && sudo ./llvm.sh 18
+sudo apt-get install libc++-18-dev
+python3 -mpip install meson==1.4.0 ninja
+sudo apt-get install libsecp256k1-dev -y
+git submodule update --init
 mkdir subprojects
-meson wrap install nlohmann_json && meson setup build && cd build && ninja SoB && ./SoB
+meson wrap install nlohmann_json
+
+CXX=clang++-18 CC=clang-18 meson setup build && cd build && ninja SoB && ./SoB
